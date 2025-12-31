b3668a2c83e6ec116952816e8556026259623f72
RAYXSTUDIOZGAMES <146851387+RAYXSTUDIOZGAMES@users.noreply.github.com>
2025-12-31 16:50:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/black-hole-portfolio/', // IMPORTANT: Replace this with your repository name if it differs
+  base: '/', 
 })
